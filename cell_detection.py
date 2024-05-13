86d4b70aa1b0987c2a937d6f222cc593cf8956c7
Nikodem Iwin <medokingry@gmail.com>
2024-05-14 00:07:21 +0200

    updated laoding files and filling_map

--- cell_detection.py
+++ cell_detection.py
@@ -2,12 +2,15 @@ import numpy as np
 import csv
 from pathlib import Path
 import copy
+import matplotlib.pyplot as plt
 
-MAX_LENGTH = 140
-MAX_WIDTH = 140
-CENTER = 70
+MAX_LENGTH = 250
+MAX_WIDTH = 250
+CENTER_X = int(MAX_WIDTH/2)
+CENTER_Y = int(MAX_LENGTH/2)
 
-RESOLUTION = 0.1
+#Ze względu na wielkość point clouda muszę dać większą rozdzieczość
+RESOLUTION = 1
 
 memory_ogm = np.zeros((MAX_WIDTH, MAX_LENGTH))
 new_ogm = copy.deepcopy(memory_ogm)
@@ -19,8 +22,42 @@ def load_csv(path_to_file: Path) -> np.array:
         data = csv.reader(csvfile, delimiter=',')
         for row in data:
             row = [float(i) for i in row]
-            data_list.append(np.array(row))
 
+            #Magical geometry math
+            row[0] = 50 - row[0]
+            row[1] = row[1] - 45
+            # Z is irrelevant
+            # row = [a - b for a,b in zip([50, 45, 50], row)] #Camera Coords
+            data_list.append(np.array(row))
     return np.array(data_list)     
 
-scans = load_csv(Path("csvs/custom_room_closed/0.csv"))
+
+def fill_map_with_data(map, data):
+    for point in data:
+        x = int(point[0]/RESOLUTION) + CENTER_X
+        y = int(point[1]/RESOLUTION) + CENTER_Y
+        map[x,y] = 100
+    return map
+
+scans = load_csv(Path("csvs/custom_room_opened/0.csv"))
+memory_ogm = fill_map_with_data(memory_ogm, scans)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.imshow(memory_ogm, interpolation="nearest",cmap='Blues')
+plt.show()
+
+scans_new = load_csv(Path("csvs/custom_room_closed/0.csv"))
+new_ogm = fill_map_with_data(new_ogm, scans_new)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.imshow(new_ogm, interpolation="nearest",cmap='Greens')
+plt.show()
+
+differece_map = []
+for new_row, memory_row in zip(new_ogm, memory_ogm):
+    differece_row = [a - b for a,b in zip(new_row, memory_row)]
+    differece_map.append(differece_row)
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.imshow(differece_map, interpolation="nearest",cmap='grey')
+plt.show()
