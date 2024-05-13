87d0bd6f1045dc4ff41f89d7e635526a0a2e45d4
Nikodem Iwin <medokingry@gmail.com>
2024-05-14 00:32:44 +0200

    Updated cell localisation

--- cell_detection.py
+++ cell_detection.py
@@ -4,7 +4,8 @@ from pathlib import Path
 import copy
 import matplotlib.pyplot as plt
 
-MAX_LENGTH = 250
+#L,W,R : 150,150,1
+MAX_LENGTH = 250 
 MAX_WIDTH = 250
 CENTER_X = int(MAX_WIDTH/2)
 CENTER_Y = int(MAX_LENGTH/2)
@@ -36,28 +37,57 @@ def fill_map_with_data(map, data):
     for point in data:
         x = int(point[0]/RESOLUTION) + CENTER_X
         y = int(point[1]/RESOLUTION) + CENTER_Y
-        map[x,y] = 100
+        map[x,y] = 1
     return map
 
 scans = load_csv(Path("csvs/custom_room_opened/0.csv"))
 memory_ogm = fill_map_with_data(memory_ogm, scans)
-plt.xlabel('X')
-plt.ylabel('Y')
+plt.xlabel('Y')
+plt.ylabel('X')
 plt.imshow(memory_ogm, interpolation="nearest",cmap='Blues')
 plt.show()
 
 scans_new = load_csv(Path("csvs/custom_room_closed/0.csv"))
 new_ogm = fill_map_with_data(new_ogm, scans_new)
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.imshow(new_ogm, interpolation="nearest",cmap='Greens')
+plt.xlabel('Y')
+plt.ylabel('X')
+plt.imshow(new_ogm, interpolation="nearest",cmap='Blues')
 plt.show()
 
+print("Case Open->Close (Find 1)")
 differece_map = []
 for new_row, memory_row in zip(new_ogm, memory_ogm):
     differece_row = [a - b for a,b in zip(new_row, memory_row)]
     differece_map.append(differece_row)
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.imshow(differece_map, interpolation="nearest",cmap='grey')
+
+for i in range(MAX_WIDTH):
+    for j in range(MAX_LENGTH):
+        if differece_map[i][j] == 1:
+            print("Detected doors in place: " + str(i+1) + " x " + str(j+1))
+
+plt.xlabel('Y')
+plt.ylabel('X')
+# plt.imshow(differece_map, interpolation="nearest",cmap='grey')
+# plt.colorbar()
+plt.imshow(differece_map)
+plt.show()
+
+
+
+print("Case Close->Open (Find -1)")
+differece_map = []
+for new_row, memory_row in zip(new_ogm, memory_ogm):
+    differece_row = [b - a for a,b in zip(new_row, memory_row)]
+    differece_map.append(differece_row)
+
+for i in range(MAX_WIDTH):
+    for j in range(MAX_LENGTH):
+        if differece_map[i][j] == -1:
+            print("Detected doors in place: " + str(i+1) + " x " + str(j+1))
+
+plt.xlabel('Y')
+plt.ylabel('X')
+# plt.imshow(differece_map, interpolation="nearest",cmap='grey')
+# plt.colorbar()
+plt.imshow(differece_map)
 plt.show()
