fffd8c9e6fb594851b5b172d9287ed2e6af2b846
Nikodem Iwin <medokingry@gmail.com>
2024-06-05 00:52:02 +0200

    Created segmentation for all files in folder

--- folder_segmentation.py
+++ folder_segmentation.py
@@ -5,7 +5,13 @@ from scipy.ndimage import label
 
 DOORS_THRESHOLD = 0.49
 P_INIT = 0.5
-MAPS_FOLDER = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/lab15_simulation"
+# MAPS_FOLDER = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/lab15_simulation"
+# MAPS_FOLDER = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/lab15_sim_rotated"
+MAPS_FOLDER = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/lab15_real"
+
+# PATH_TO_SAVE = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/simulation_doors.png"
+# PATH_TO_SAVE = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/simulation_doors_rotated.png"
+PATH_TO_SAVE = "/home/nikodem/Documents/door-detection-with-ogm/folder_segmentation/real_doors.png"
 TOLERANCE = 1e-6
 
 def load_maps_from_folder():
@@ -27,14 +33,16 @@ def init_result_map(maps):
         max_columns = max(max_columns, columns)
     
     # map = np.full((max_rows, max_columns), P_INIT, dtype=np.float32)
-    map = np.empty([max_rows,max_columns])
+    map = np.zeros([max_rows,max_columns])
     return map 
 
 
 def display_maps(maps):
     i = 1
+    plt.figure(figsize=(15, 5))
     for map in maps:
         plt.subplot(2, 3, i)
+        plt.scatter(50, 50, color="red", label='Lidar', s=10)
         plt.imshow(map, cmap='gray_r')
         plt.title(f'map {i}')
         plt.colorbar()
@@ -42,53 +50,83 @@ def display_maps(maps):
     
     plt.show()
 
+def sanitize_map(map):
+    map[np.isinf(map)] = 0
+    map = np.nan_to_num(map)
+    return map
 
 def segmentation(binary_mask):
-    labeled_array, num_features = label(binary_mask)
+    pattern = [[1,1,1],
+               [1,1,1],
+               [1,1,1]]
+    labeled_array, num_features = label(binary_mask, pattern)
     segments = []
     
     for i in range(1, num_features + 1):
         segment = np.argwhere(labeled_array == i)
-        if len(segment) >= 5:  # Filter out small segments
+        if len(segment) >= 3:  # Filter out small segments
             segments.append(segment)
     
     return segments, labeled_array
 
+def remove_duplicates(list_of_arrays):
+    seen = set()
+    result = []
+    for array in list_of_arrays:
+        tup = tuple(map(tuple, array))
+        if tup not in seen:
+            seen.add(tup)
+            result.append(array)
+    return result
 
 def main():
     maps = load_maps_from_folder()
     result_map = init_result_map(maps)
-    # display_maps(maps)
+    background =np.copy(result_map)
+    display_maps(maps)
 
     # plt.imshow(maps[0] - maps[1], cmap='gray_r')
     # plt.title('??? map')
     # plt.colorbar()
     # plt.show()
-    index = 1
-    for map in maps: 
+    # value = 1
+
+    segments_list = []
+    for map in maps:
+        reflections = np.where(map > 0.9, 1, 0)
+        for point in np.argwhere(reflections == 1):
+            background[point[0], point[1]] = 1 
+
         for map2 in maps:
             if (map == map2).all():
                 continue
             else:
                 difference_map = np.absolute(map - map2)
+                difference_map = sanitize_map(difference_map)
                 binary_map = np.where(difference_map - DOORS_THRESHOLD > TOLERANCE, 1, 0)
+                binary_map = sanitize_map(binary_map)
 
-                # plt.subplot(2, 2, 1)
+                # plt.figure(figsize=(15, 5))
+                # plt.subplot(1, 4, 1)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
                 # plt.imshow(map, cmap='gray_r')
                 # plt.title('Map 1')
                 # plt.colorbar()
 
-                # plt.subplot(2, 2, 2)
+                # plt.subplot(1, 4, 2)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
                 # plt.imshow(map2, cmap='gray_r')
                 # plt.title('Map 2')
                 # plt.colorbar()
 
-                # plt.subplot(2, 2, 3)
+                # plt.subplot(1, 4, 3)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
                 # plt.imshow(difference_map, cmap='gray_r')
                 # plt.title('Difference map')
                 # plt.colorbar()
 
-                # plt.subplot(2, 2, 4)
+                # plt.subplot(1, 4, 4)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
                 # plt.imshow(binary_map, cmap='gray_r')
                 # plt.title('Binary map')
                 # plt.colorbar()
@@ -100,35 +138,76 @@ def main():
                 cmap.set_under('white')
                 colors = [cmap(i) for i in range(len(segments))]
 
-                plt.subplot(1, 3, 1)
-                plt.imshow(difference_map, cmap='gray_r')
-                plt.scatter(50, 50, color="red", label='Lidar', s=10)
-                for i, segment in enumerate(segments):
-                    x_coords, y_coords = zip(*segment)
-                    plt.scatter(y_coords, x_coords, color=colors[i], label=f'Segment {i+1}', s=10)
-                plt.title('Map with segments')
+                # plt.figure(figsize=(15, 5))
+                # plt.subplot(1, 4, 1)
+                # plt.imshow(difference_map, cmap='gray_r')
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
+                # for i, segment in enumerate(segments):
+                #     x_coords, y_coords = zip(*segment)
+                #     plt.scatter(y_coords, x_coords, color=colors[i], label=f'Segment {i+1}', s=10)
+                # plt.title('Map with segments')
 
 
-                plt.subplot(1, 3, 2)
-                plt.imshow(segment_labels, cmap='gray_r')
-                plt.title('segment_labels')
+                # plt.subplot(1, 4, 2)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
+                # plt.imshow(segment_labels, cmap='gray_r')
+                # plt.title('segment_labels')
 
                 for segment in segments:
-                    index_flag = False
                     for point in segment:
                         if result_map[point[0], point[1]] == 0:
-                            result_map[point[0], point[1]] = 1#index
-                    #         index_flag = True
-                    # if index_flag is True:
-                    #     index += 1
+                            result_map[point[0], point[1]] = 1
+
+                    flag = True
+                    for noted_segment in segments_list:
+                        if np.array_equal(segment, noted_segment, equal_nan=False):
+                            flag = False
                     
+                    if flag is True:
+                        segments_list.append(segment)
 
+                # plt.subplot(1, 4, 3)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
+                # plt.imshow(result_map, cmap='gray_r')
+                # plt.title('result_map')
+                # # plt.show()
 
-                plt.subplot(1, 3, 3)
-                plt.imshow(result_map, cmap='gray_r')
-                plt.title('result_map')
-                plt.colorbar()
-                plt.show()
+                # plt.subplot(1, 4, 4)
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
 
+                segments, segment_labels = segmentation(result_map)
+
+                cmap = plt.get_cmap('summer', len(segments))
+                cmap.set_under('white')
+                colors = [cmap(i) for i in range(len(segments))]
+
+                # plt.imshow(difference_map, cmap='gray_r')
+                # plt.scatter(50, 50, color="red", label='Lidar', s=10)
+                # for i, segment in enumerate(segments):
+                #     x_coords, y_coords = zip(*segment)
+                #     plt.scatter(y_coords, x_coords, color=colors[i], label=f'Segment {i+1}', s=10)
+                # plt.title('Map segments_list')
+                # plt.show()
+
+    print(len(segments_list))
+    segments_list = remove_duplicates(segments_list)
+    print(len(segments_list))
+
+    plt.scatter(50, 50, color="red", label='Lidar', s=10)
+
+    segments, segment_labels = segmentation(result_map)
+
+    cmap = plt.get_cmap('summer', len(segments))
+    cmap.set_under('white')
+    colors = [cmap(i) for i in range(len(segments))]
+
+    plt.imshow(background, cmap='gray_r')
+    plt.scatter(50, 50, color="red", label='Lidar', s=10)
+    for i, segment in enumerate(segments):
+        x_coords, y_coords = zip(*segment)
+        plt.scatter(y_coords, x_coords, color=colors[i], label=f'Segment {i+1}', s=10)
+    plt.title('DETECTED AREAS')
+    plt.savefig(PATH_TO_SAVE)
+    plt.show()         
 if __name__ == "__main__":
     main()
