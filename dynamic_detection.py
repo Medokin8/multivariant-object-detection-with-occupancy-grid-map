d294668e475736d1daa08e347b4efac58cc5445f
Nikodem Iwin <medokingry@gmail.com>
2024-06-15 01:21:28 +0200

    Updates for needs of thesis

--- dynamic_detection.py
+++ dynamic_detection.py
@@ -22,22 +22,43 @@ MIN_NUMBER_OF_CELLS = 3
 P_THRESHOLD = 0.49
 TOLERANCE = 1e-6
 
-TOPIC = "/laser_scan"  # "/pioneer1/scan"
-FRAME_ID = "sim_lidar"  # "pioneer1/laser"
+TOPIC = "/pioneer1/scan"
+FRAME_ID = "pioneer1/laser"
 
 
 class LaserListenerNode(Node):
 
     def __init__(self):
         super().__init__("laser_listener_node")
-        self.subscription = self.create_subscription(LaserScan, TOPIC, self.listener_callback, 10)
-        self.grid_publisher = self.create_publisher(OccupancyGrid, "occupancy_grid", 10)
-        self.change_publisher = self.create_publisher(OccupancyGrid, "detected_objects", 10)
+        self.subscription = self.create_subscription(
+            LaserScan,
+            TOPIC,
+            self.listener_callback,
+            10,
+        )
+        self.grid_publisher = self.create_publisher(
+            OccupancyGrid,
+            "occupancy_grid",
+            10,
+        )
+        self.change_publisher = self.create_publisher(
+            OccupancyGrid,
+            "detected_objects",
+            10,
+        )
 
         self.timer = self.create_timer(1.0, self.publish_occupancy_grid)
 
-        self.grid_map = np.full((GRID_SIZE, GRID_SIZE), P_INIT, dtype=np.float32)
-        self.memory_map = np.full((GRID_SIZE, GRID_SIZE), P_INIT, dtype=np.float32)
+        self.grid_map = np.full(
+            (GRID_SIZE, GRID_SIZE),
+            P_INIT,
+            dtype=np.float32,
+        )
+        self.memory_map = np.full(
+            (GRID_SIZE, GRID_SIZE),
+            P_INIT,
+            dtype=np.float32,
+        )
         self.change_map = np.zeros((GRID_SIZE, GRID_SIZE), dtype=np.int8)
         self.blobs = np.zeros((GRID_SIZE, GRID_SIZE), dtype=np.int8)
 
@@ -58,10 +79,18 @@ class LaserListenerNode(Node):
         while True:
             if 0 <= x0 < GRID_SIZE and 0 <= y0 < GRID_SIZE:
                 if x0 != x1 or y0 != y1:
-                    probability = np.log(self.grid_map[x0, y0] / (1 - self.grid_map[x0, y0]))
+                    probability = np.log(
+                        self.grid_map[x0, y0] / (1 - self.grid_map[x0, y0])
+                    )
                     new_probability = probability + LOG_ODDS_FREE
-                    self.grid_map[x0, y0] = 1 - 1 / (1 + np.exp(new_probability))
-                    self.grid_map[x0, y0] = np.clip(self.grid_map[x0, y0], P_MIN, P_MAX)
+                    self.grid_map[x0, y0] = 1 - 1 / (
+                        1 + np.exp(new_probability)
+                    )
+                    self.grid_map[x0, y0] = np.clip(
+                        self.grid_map[x0, y0],
+                        P_MIN,
+                        P_MAX,
+                    )
 
             if x0 == x1 and y0 == y1:
                 break
@@ -80,7 +109,11 @@ class LaserListenerNode(Node):
         return map
 
     def segmentation(self, binary_mask):
-        pattern = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+        pattern = [
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+        ]
 
         labeled_array, num_features = label(binary_mask, pattern)
         segments = []
@@ -107,7 +140,9 @@ class LaserListenerNode(Node):
         max_similarity = 0
         most_similar_segment = None
         for segment in segments:
-            common_points = source_points_set.intersection(set(tuple(point) for point in segment))
+            common_points = source_points_set.intersection(
+                set(tuple(point) for point in segment)
+            )
             similarity = len(common_points)
             if similarity > max_similarity:
                 max_similarity = similarity
@@ -121,7 +156,11 @@ class LaserListenerNode(Node):
 
         for segment in segment_list:
             segment_points = [tuple(point) for point in segment]
-            unique_points = [point for point in segment_points if point not in included_points]
+            unique_points = [
+                point
+                for point in segment_points
+                if point not in included_points
+            ]
             unique_segment = np.array(unique_points)
             if len(unique_segment) > 1:
                 output_segments.append(unique_segment)
@@ -129,10 +168,21 @@ class LaserListenerNode(Node):
             elif len(unique_segment) == 1:
                 single_point_segments.append((unique_segment[0], segment))
 
-        for unique_point, source_segment in single_point_segments:
-            most_similar_segment = self.find_most_similar_segment(source_segment, output_segments)
+        for (
+            unique_point,
+            source_segment,
+        ) in single_point_segments:
+            most_similar_segment = self.find_most_similar_segment(
+                source_segment,
+                output_segments,
+            )
             if most_similar_segment is not None:
-                most_similar_segment = np.vstack([most_similar_segment, unique_point])
+                most_similar_segment = np.vstack(
+                    [
+                        most_similar_segment,
+                        unique_point,
+                    ]
+                )
             else:
                 output_segments.append(np.array([unique_point]))
 
@@ -141,7 +191,11 @@ class LaserListenerNode(Node):
     def check_for_changes(self):
         difference_map = np.absolute(self.grid_map - self.memory_map)
         difference_map = self.sanitize_map(difference_map)
-        binary_map = np.where(difference_map > TOLERANCE, 1, 0)
+        binary_map = np.where(
+            difference_map - P_THRESHOLD > TOLERANCE,
+            1,
+            0,
+        )
         binary_map = self.sanitize_map(binary_map)
 
         segments, _ = self.segmentation(binary_map)
@@ -153,7 +207,11 @@ class LaserListenerNode(Node):
 
             flag = True
             for noted_segment in self.segments_list:
-                if np.array_equal(segment, noted_segment, equal_nan=False):
+                if np.array_equal(
+                    segment,
+                    noted_segment,
+                    equal_nan=False,
+                ):
                     flag = False
 
             if flag is True:
@@ -171,7 +229,11 @@ class LaserListenerNode(Node):
 
     def listener_callback(self, msg):
         for i, range in enumerate(msg.ranges):
-            if range < msg.range_min or range > msg.range_max or np.isnan(range):
+            if (
+                range < msg.range_min
+                or range > msg.range_max
+                or np.isnan(range)
+            ):
                 continue
 
             angle = msg.angle_min + i * msg.angle_increment
@@ -181,13 +243,27 @@ class LaserListenerNode(Node):
             grid_x = int(self.origin_x + x / CELL_SIZE)
             grid_y = int(self.origin_y + y / CELL_SIZE)
 
-            self.bresenham(self.origin_x, self.origin_y, grid_x, grid_y)
+            self.bresenham(
+                self.origin_x,
+                self.origin_y,
+                grid_x,
+                grid_y,
+            )
 
             if 0 <= grid_x < GRID_SIZE and 0 <= grid_y < GRID_SIZE:
-                old_probability = np.log(self.grid_map[grid_x, grid_y] / (1 - self.grid_map[grid_x, grid_y]))
+                old_probability = np.log(
+                    self.grid_map[grid_x, grid_y]
+                    / (1 - self.grid_map[grid_x, grid_y])
+                )
                 new_probability = old_probability + LOG_ODDS_OCCUPIED
-                self.grid_map[grid_x, grid_y] = 1 - 1 / (1 + np.exp(new_probability))
-                self.grid_map[grid_x, grid_y] = np.clip(self.grid_map[grid_x, grid_y], P_MIN, P_MAX)
+                self.grid_map[grid_x, grid_y] = 1 - 1 / (
+                    1 + np.exp(new_probability)
+                )
+                self.grid_map[grid_x, grid_y] = np.clip(
+                    self.grid_map[grid_x, grid_y],
+                    P_MIN,
+                    P_MAX,
+                )
 
         self.check_for_changes()
         self.last_scan_time = msg.header.stamp
@@ -211,7 +287,9 @@ class LaserListenerNode(Node):
         occupancy_grid.info.origin.position.y = -(GRID_SIZE // 2) * CELL_SIZE
         occupancy_grid.info.origin.position.z = 0.0
         occupancy_grid.info.origin.orientation.w = 1.0
-        occupancy_grid.data = (rotated_map * 100).astype(np.int8).flatten().tolist()
+        occupancy_grid.data = (
+            (rotated_map * 100).astype(np.int8).flatten().tolist()
+        )
         self.grid_publisher.publish(occupancy_grid)
         self.get_logger().info("Occupancy grid published")
 
@@ -236,7 +314,9 @@ class LaserListenerNode(Node):
         change_grid.info.origin.position.y = -(GRID_SIZE // 2) * CELL_SIZE
         change_grid.info.origin.position.z = 0.0
         change_grid.info.origin.orientation.w = 1.0
-        change_grid.data = (rotated_change_map * 2).astype(np.int8).flatten().tolist()
+        change_grid.data = (
+            (rotated_change_map * 2).astype(np.int8).flatten().tolist()
+        )
 
         self.change_publisher.publish(change_grid)
         self.get_logger().info("Detected objects published")
--- ogm_generator.py
+++ ogm_generator.py
@@ -16,18 +16,27 @@ LOG_ODDS_OCCUPIED = np.log(P_OCCUPIED / (1 - P_OCCUPIED))
 LOG_ODDS_FREE = np.log(P_FREE / (1 - P_FREE))
 
 TOPIC = "/pioneer1/scan"
+PATH_AND_FILE_TO_SAVE = "tmp/tmp"
 
 
 class LaserListenerNode(Node):
 
     def __init__(self):
         super().__init__("laser_listener_node")
-        self.subscription = self.create_subscription(LaserScan, TOPIC, self.listener_callback, 10)
+        self.subscription = self.create_subscription(
+            LaserScan,
+            TOPIC,
+            self.listener_callback,
+            10,
+        )
 
         self.timer = self.create_timer(1.0, self.check_for_topic)
 
-        self.grid_map = np.full((GRID_SIZE, GRID_SIZE), P_INIT, dtype=np.float32)
-        self.memory_map = np.full((GRID_SIZE, GRID_SIZE), P_INIT, dtype=np.float32)
+        self.grid_map = np.full(
+            (GRID_SIZE, GRID_SIZE),
+            P_INIT,
+            dtype=np.float32,
+        )
 
         self.origin_x = GRID_SIZE // 2
         self.origin_y = GRID_SIZE // 2
@@ -44,10 +53,18 @@ class LaserListenerNode(Node):
         while True:
             if 0 <= x0 < GRID_SIZE and 0 <= y0 < GRID_SIZE:
                 if x0 != x1 or y0 != y1:
-                    probability = np.log(self.grid_map[x0, y0] / (1 - self.grid_map[x0, y0]))
+                    probability = np.log(
+                        self.grid_map[x0, y0] / (1 - self.grid_map[x0, y0])
+                    )
                     new_probability = probability + LOG_ODDS_FREE
-                    self.grid_map[x0, y0] = 1 - 1 / (1 + np.exp(new_probability))
-                    self.grid_map[x0, y0] = np.clip(self.grid_map[x0, y0], P_MIN, P_MAX)
+                    self.grid_map[x0, y0] = 1 - 1 / (
+                        1 + np.exp(new_probability)
+                    )
+                    self.grid_map[x0, y0] = np.clip(
+                        self.grid_map[x0, y0],
+                        P_MIN,
+                        P_MAX,
+                    )
 
             if x0 == x1 and y0 == y1:
                 break
@@ -62,7 +79,11 @@ class LaserListenerNode(Node):
 
     def listener_callback(self, msg):
         for i, range in enumerate(msg.ranges):
-            if range < msg.range_min or range > msg.range_max or np.isnan(range):
+            if (
+                range < msg.range_min
+                or range > msg.range_max
+                or np.isnan(range)
+            ):
                 continue
 
             angle = msg.angle_min + i * msg.angle_increment
@@ -72,28 +93,51 @@ class LaserListenerNode(Node):
             grid_x = int(self.origin_x + x / CELL_SIZE)
             grid_y = int(self.origin_y + y / CELL_SIZE)
 
-            self.bresenham(self.origin_x, self.origin_y, grid_x, grid_y)
+            self.bresenham(
+                self.origin_x,
+                self.origin_y,
+                grid_x,
+                grid_y,
+            )
 
             if 0 <= grid_x < GRID_SIZE and 0 <= grid_y < GRID_SIZE:
-                old_probability = np.log(self.grid_map[grid_x, grid_y] / (1 - self.grid_map[grid_x, grid_y]))
+                old_probability = np.log(
+                    self.grid_map[grid_x, grid_y]
+                    / (1 - self.grid_map[grid_x, grid_y])
+                )
                 new_probability = old_probability + LOG_ODDS_OCCUPIED
-                self.grid_map[grid_x, grid_y] = 1 - 1 / (1 + np.exp(new_probability))
-                self.grid_map[grid_x, grid_y] = np.clip(self.grid_map[grid_x, grid_y], P_MIN, P_MAX)
+                self.grid_map[grid_x, grid_y] = 1 - 1 / (
+                    1 + np.exp(new_probability)
+                )
+                self.grid_map[grid_x, grid_y] = np.clip(
+                    self.grid_map[grid_x, grid_y],
+                    P_MIN,
+                    P_MAX,
+                )
 
         self.last_scan_time = msg.header.stamp
 
     def check_for_topic(self):
         topics = self.get_topic_names_and_types()
         if not any(TOPIC in topic for topic, types in topics):
-            self.get_logger().info("/laser_scan topic not found, shutting down...")
+            self.get_logger().info(
+                "/laser_scan topic not found, shutting down..."
+            )
             rclpy.shutdown()
 
     def save_memory_map(self):
-        folder_and_name = "folder_segmentation/lab15_real/lab15_side_opened"
+        PATH_AND_FILE_TO_SAVE
         probabilities = self.grid_map
-        np.save(folder_and_name + ".npy", probabilities)
+        np.save(
+            PATH_AND_FILE_TO_SAVE + ".npy",
+            probabilities,
+        )
         self.get_logger().info("Memory map saved to memory_map.npy")
-        np.savetxt(folder_and_name + ".csv", probabilities, delimiter=",")
+        np.savetxt(
+            PATH_AND_FILE_TO_SAVE + ".csv",
+            probabilities,
+            delimiter=",",
+        )
         self.get_logger().info("Memory map saved to memory_map.csv")
 
 
